6b29d02a0c9f9fa08826a20ee66eb7002b578868
Dustin Peukert <dpeukert04@gmail.com>
2024-09-19 19:41:40 -0400

    Simplified code on line 2

--- code_challenges/mild_challenge.rb
+++ code_challenges/mild_challenge.rb
@@ -1,5 +1,5 @@
-number = 7 # declares number as int with a value of 7
-if number % 2 == 0 # determines the remainder, if 0 then number is even, otherwise it's odd
+number = 8 # declares number as int with a value of 7
+if number.even?
     print number, " is even." 
 else
     print number, " is odd."
