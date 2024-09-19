c7220f7f2be1dfd6fb7444e14d009c4ebba5d9f6
Dustin Peukert <dpeukert04@gmail.com>
2024-09-19 19:39:56 -0400

    Add script to determine odd or even value

--- code_challenges/mild_challenge.rb
+++ code_challenges/mild_challenge.rb
@@ -1,3 +1,6 @@
-number = 7
-if number % 2 == 0
-    puts ""
+number = 7 # declares number as int with a value of 7
+if number % 2 == 0 # determines the remainder, if 0 then number is even, otherwise it's odd
+    print number, " is even." 
+else
+    print number, " is odd."
+end
